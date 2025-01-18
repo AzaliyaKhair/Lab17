0d816d5b2399214965e8575e579dddb9346cf084
Azaliya <azaliya.pochta@gmail.com>
2025-01-18 17:41:06 +0300

    ok

--- src/main/java/ru/itis11403/CheckUser.java
+++ src/main/java/ru/itis11403/CheckUser.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 class CheckUser {
-    private User admin = new User(Role.ADMIN, "adminLogin@example.com", "AdminPassword962{}");
-    private User user = new User(Role.USER, "userLogin@example.com", "UserPassword998[]");
+    private User admin = new User(Role.ADMIN, "Azaliya.pochta786@gmailcom", "Cool17[]Q");
+    private User user = new User(Role.USER, "Cool17[]Q", "UserPassword998[]");
 
     private boolean isValidLogin(String login) {
         if (login.length() < 20) return false;
@@ -16,10 +16,29 @@ class CheckUser {
 
     private boolean isValidPassword(String password) {
         if (password.length() < 8) return false;
-        Pattern pattern = Pattern.compile("^[a-zA-Z0-9\\{\\[\\]\\(\\),.;&?!_-+]{8,}$");
+        Pattern pattern = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[{}\\[\\](),.;&?!_\\-+]).{8,}$");
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
 
     }
+
+    public static void displayMenu(Role role) {
+        switch (role) {
+            case ADMIN:
+                System.out.println("\n--- ADMIN MENU ---");
+                System.out.println("1. File");
+                System.out.println("2. Create new user");
+                System.out.println("3. Exit");
+                break;
+
+            case USER:
+                System.out.println("\n--- USER MENU ---");
+                System.out.println("1. File");
+                System.out.println("2. Get play list");
+                System.out.println("3. Exit");
+                break;
+        }
+    }
+
 }
 
